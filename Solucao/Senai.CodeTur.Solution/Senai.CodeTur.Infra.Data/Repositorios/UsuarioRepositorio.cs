43ab8b3e5b830cd64b3983531f7d8e872eb3b9cf
Murilo Milani <murilomilani8870@gmail.com>
2020-01-31 11:08:46 -0300

    Criacao do Banco e da solucao 31/01/2020T11:08

---
+++ Solucao/Senai.CodeTur.Solution/Senai.CodeTur.Infra.Data/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using Senai.CodeTur.Dominio.Entidades;
+using Senai.CodeTur.Dominio.Interfaces.Repositorios;
+using Senai.CodeTur.Infra.Data.Contextos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senai.CodeTur.Infra.Data.Repositorios
+{
+    public class UsuarioRepositorio: IUsuarioRepositorio
+    {
+        public UsuarioDominio EfetuarLogin(string email, string senha)
+        {
+            try
+            {
+                using (CodeTurContext ctx = new CodeTurContext())
+                {
+                    return ctx.Usuarios.FirstOrDefault(x => x.Email == email && x.Senha == senha);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+        }
+ 
+    }
+}
---
+++ Solucao/Senai.CodeTur.Solution/Senai.CodeTur.Teste.XUnit/Repositorios/TesteUsuario.cs
@@ -0,0 +1,52 @@
+﻿using Senai.CodeTur.Dominio.Entidades;
+using Senai.CodeTur.Dominio.Interfaces.Repositorios;
+using Senai.CodeTur.Infra.Data.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Senai.CodeTur.Teste.XUnit.Repositorios
+{
+    public class TesteUsuario
+    {
+        private IUsuarioRepositorio _repositorioUsuario;
+
+        public TesteUsuario()
+        {
+            _repositorioUsuario = new UsuarioRepositorio();
+        }
+
+        [Fact]
+        public void VerficaUsuarioEInvalido()
+        {
+            var validacao = _repositorioUsuario.EfetuarLogin("admin@gmail.com", "12345");
+
+            Assert.Null(validacao);
+        }
+
+        [Fact]
+        public void VerificaSeUsuarioEValido()
+        {
+            var validacao = _repositorioUsuario.EfetuarLogin("admin@codetur.com", "Codetur@132");
+
+            Assert.NotNull(validacao);
+        }
+
+        [Fact]
+        public void VerificarSeUsuarioEValidoEInfoCorretas()
+        {
+            UsuarioDominio usuario = new UsuarioDominio()
+            {
+                Email = "admin@codetur.com",
+                Senha = "Codetur@132"
+            };
+
+            UsuarioDominio usuarioRetorno = _repositorioUsuario
+                                                .EfetuarLogin(usuario.Email, usuario.Senha);
+
+            Assert.Equal(usuarioRetorno.Email, usuario.Email);
+            Assert.Equal(usuarioRetorno.Senha, usuario.Senha);
+        }
+    }
+}
