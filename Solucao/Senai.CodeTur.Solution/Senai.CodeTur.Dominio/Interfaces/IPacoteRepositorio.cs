7cf7ec8a2926c067129a9cca71778163e58addff
Murilo Milani <murilomilani8870@gmail.com>
2020-01-31 11:28:06 -0300

    Inicio dos Pacotes 31/01/2020T11:28

---
+++ Solucao/Senai.CodeTur.Solution/Senai.CodeTur.Dominio/Interfaces/IPacoteRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Senai.CodeTur.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senai.CodeTur.Dominio.Interfaces
+{
+    public interface IPacoteRepositorio
+    {
+        List<PacoteDominio> Listar(bool? todos = null);
+
+        PacoteDominio BuscarPorId(int id);
+
+        PacoteDominio Cadastrar(PacoteDominio pacote);
+
+    }
+}
---
+++ Solucao/Senai.CodeTur.Solution/Senai.CodeTur.Infra.Data/Repositorios/PacoteRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senai.CodeTur.Infra.Data.Repositorios
+{
+    class PacoteRepositorio
+    {
+        public PacoteDominio Cadastrar(PacoteDominio pacote)
+    }
+}
